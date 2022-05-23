06af4d2db9e52acd0999f3b9293695e02e2fb98c
João Pedro Almeida <53056229+joao0812@users.noreply.github.com>
2022-05-23 15:38:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var hr = 0
 var min = 0
 var seg = 0
 
+var volta = 0
+
 var tempo = 1000;
 var cron;
 
@@ -20,16 +22,19 @@ function Stop() {
     hr = 0
     min = 0
     seg = 0
+    volta = 0
 
     document.getElementById('cron').innerHTML = '<h1> 00:00:00 </h1>'
+
+    document.getElementById('volta').innerHTML = '<h3> VOLTAS </h3>'
 }
 
 function timer() {
     seg++
-    if(seg == 60){
+    if (seg == 60) {
         seg = 0
         min++
-        if(min == 60){
+        if (min == 60) {
             min = 0
             hr++
         }
@@ -37,4 +42,12 @@ function timer() {
 
     var format = (hr < 10 ? '0' + hr : hr) + ':' + (min < 10 ? '0' + min : min) + ':' + (seg < 10 ? '0' + seg : seg)
     document.getElementById("cron").innerHTML = '<h1>' + format + '</h1>'
+    return format
+}
+
+function Volta() {
+    var format = timer()
+    volta++
+    document.getElementById('volta').innerHTML += '<br>' + '<h2>' + volta + '</h2>' + '<h3>' + format + '</h3>'
+
 }
